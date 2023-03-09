7c553ee908de67ca4dbb92f6ec3909650a17473c
JensMatthiaschk <jens.matthiaschk@gmail.com>
2023-03-09 17:26:48 +0100

    Add image lightbox

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Expletus_Sans } from 'next/font/google';
+
+const expletus = Expletus_Sans({
+  subsets: ['latin'],
+  variable: '--font-expletus',
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={`${expletus.variable} font-expletus h-full`}>
+      <Component {...pageProps} />
+    </div>)
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        expletus: ['--font-expletus', ...fontFamily.sans]
+      },
+    },
   },
   plugins: [],
 }
